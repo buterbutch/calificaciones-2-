bf73c80d58b228df638f716b406d0a56c0b323a4
buterbutch <87133390+buterbutch@users.noreply.github.com>
2021-07-27 22:34:17 -0500

    Delete Javascript directory
    se eilimino

--- Javascript/Fecha.js
+++
@@ -1,9 +0,0 @@
-let hoy =new Date ();
-let dia =hoy.getDate();
-let mes =hoy.getMonth() + 1;
-let year =hoy.getfullyear();
-
-//DD-MM-AAAA
-
-let fecha1 = 'dia + mes + year';
-document.write(fecha1);
--- Javascript/alerts.js
+++
@@ -1,6 +0,0 @@
-/*  alert("Cuidémonos frente a la pandemia: sigue las recomendaciones en tu área"); // esto hace que se genere una alerta//
-var nombre; //para añadir una variable//
-
-nombre = prompt ("ingresa tu nombre");
-document.write ("Te damos la bienvenida," + " " + nombre) //devuelve los datos que se ingresaron//
- */
